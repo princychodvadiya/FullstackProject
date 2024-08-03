208d384ad2abcd3ac24580ba74082f2b0487d4ab
Akshay-Padaliya <patel.akshay43@gmail.com>
2024-08-03 09:56:27 +0530

    3-8-24

--- backend/ecommerece/src/utils/socketIO.js
+++ backend/ecommerece/src/utils/socketIO.js
@@ -12,6 +12,17 @@ const connectChat = () => {
 
         socket.emit('welcome', 'welcome to frutible.');
         socket.broadcast.emit('greeting', 'hello all.')
+
+        socket.on('message', (data) => {
+            console.log(data);
+
+            io.to(data.receiver).emit("rec-mag", data.message)
+        })
+        socket.on('group_message', (group_message) => {
+            console.log(group_message)
+            socket.join(group_message);
+        })
+
     });
 
 
--- fronted/fruitable/src/user/container/Chat/Chat.js
+++ fronted/fruitable/src/user/container/Chat/Chat.js
@@ -1,20 +1,44 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { io } from 'socket.io-client';
 
 function Chat(props) {
-    const socket = useMemo(() => io("http://localhost:8080/"));
+    const [rec, setRec] = useState('');
+    const [meg, setMeg] = useState('');
+    const [newmsg, setNewmsg] = useState([]);
+    const [group, setGroup] = useState('')
+
+    const socket = useMemo(() => io("http://localhost:8080"));
 
     useEffect(() => {
         socket.on('connect', () => {
             console.log('Connected to the server', socket.id);
-
-            socket.on('welcome', (msg) => console.log(msg));
-            socket.on('greeting', (msg) => console.log(msg))
         });
 
-    }, [])
+        socket.on('welcome', (msg) => console.log(msg));
+        socket.on('greeting', (msg) => console.log(msg));
+        // socket.on('rec-mag', (msg) => console.log(msg));
+        socket.on('rec-mag', (msg) => setNewmsg((prev) => [...prev, msg]))
 
-    
+    }, [group]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        socket.emit('message', {
+            receiver: rec,
+            message: meg
+        }
+        )
+    }
+
+    const handleGroupSubmit = (e) => {
+        e.preventDefault();
+        console.log(group);
+        socket.emit('group_message', group);
+
+
+    }
+
     return (
         <div>
             <div className="container-fluid page-header py-5">
@@ -25,6 +49,38 @@ function Chat(props) {
                     <li className="breadcrumb-item active text-white">Shop</li>
                 </ol>
             </div>
+            {
+                newmsg.map((v) => ( <p key={v}>{v}</p>
+                ))
+            }
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="pleace enter reciver_id."
+                    id='reciver'
+                    // value={props.message}
+                    onChange={(e) => setRec(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="plece enter Massage."
+                    // value={props.message}
+                    id='massage'
+                    onChange={(e) => setMeg(e.target.value)}
+                />
+                <input type='submit' />
+            </form>
+            <form onSubmit={handleGroupSubmit}>
+                <input
+                    type="text"
+                    placeholder="pleace group name."
+                    id='reciver'
+                    // value={props.message}
+                    onChange={(e) => setGroup(e.target.value)}
+                />
+                <input type='submit' />
+            </form>
+
         </div>
     );
 }
